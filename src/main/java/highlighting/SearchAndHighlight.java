130ffd7dd118d08748b72bf942e16fc5cce0754a
tinh cao <caodangtinh@gmail.com>
2018-04-03 14:59:23 +0700

    refactor code

--- src/main/java/highlighting/SearchAndHighlight.java
+++ src/main/java/highlighting/SearchAndHighlight.java
@@ -25,8 +25,8 @@ import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
 import org.apache.lucene.search.highlight.TokenSources;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
 import constant.Constant;
+
 
 public class SearchAndHighlight {
 
@@ -37,32 +37,51 @@ public class SearchAndHighlight {
 				Scanner scanner = new Scanner(System.in);
 				System.out.println("Input Your Keyword : ");
 				String keyword = scanner.nextLine();
+				
 				// Get directory reference
 				Directory directory = FSDirectory.open(Constant.INDEX_LOCATION);
+				
 				// Create index reader
 				IndexReader indexReader = DirectoryReader.open(directory);
+				
 				// Create lucene searcher. It search over a single IndexReader.
 				IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+				
 				// create analyzer with the default stop words
 				Analyzer analyzer = new StandardAnalyzer();
+				
 				// Query parser to be used for creating TermQuery
 				QueryParser queryParser = new QueryParser("contents", analyzer);
+				
 				// Create the query from input keyword
 				Query query = queryParser.parse(keyword);
+				
 				// Search the lucene documents
 				TopDocs topDocs = indexSearcher.search(query, 10);
+				
+				
 				/** Highlighter Code Start ****/
 				// Uses HTML &lt;B&gt;&lt;/B&gt; tag to highlight the searched terms
 				Formatter formatter = new SimpleHTMLFormatter();
+				
+				
 				// It scores text fragments by the number of unique query terms found
 		        // Basically the matching score in layman terms
 				QueryScorer scorer = new QueryScorer(query);
+				
+				
 				// Used to markup highlighted terms found in the best sections of a text
 				Highlighter highlighter = new Highlighter(formatter, scorer);
+				
+				
 				// It breaks text up into same-size texts but does not split up spans
 				Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
+				
+				
 				// Set fragmenter to highlighter
 				highlighter.setTextFragmenter(fragmenter);
+				
+				// get array of ScoreDoc from topDocs
 				ScoreDoc[] scoreDocs = topDocs.scoreDocs;
 				int length = scoreDocs.length;
 				// Iterate over found results
@@ -71,10 +90,14 @@ public class SearchAndHighlight {
 					Document doc = indexSearcher.doc(docId);
 					String title = doc.get("path");
 					System.out.println("Path " + " : " + title);
+					
 					// Get stored text from found document
 					String text = doc.get("contents");
+					
+					
 					// Create token stream
 					TokenStream tokenStream = TokenSources.getAnyTokenStream(indexReader, docId, "contents", analyzer);
+					
 					//Get highlighted text fragments
 					String[] frags = highlighter.getBestFragments(tokenStream, text, 10);
 					for (String frag : frags) {
